fc5319dc810c5cb1b4c1c1c413912c3367d2bd0b
haksell <axel.brisse@gmail.com>
2024-02-17 06:22:30 +0100

    finally good?

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include "gladiator.h"
+#include <algorithm>
 #include <chrono>
+#undef abs
 
-// TODO: if can launch, launch
 // TODO: all pairs shortest path
 // TODO: use previous wheel speed and update
 
 #define SIZE 12
+#define GOTO_ANGLE 0.2
+#define DFS_SIZE 6
+#define M_TAU 6.283185307179586
+#define M_HALF_PI 1.5707963267948966
 
-typedef enum e_dir {
-	NORTH = 0,
-	EAST,
-	SOUTH,
-	WEST,
-} t_dir;
+typedef enum e_dir { NORTH = 0, EAST, SOUTH, WEST, ROCKET } t_dir;
 
 Gladiator* gladiator;
 bool maze[SIZE][SIZE][4];
@@ -23,12 +23,6 @@ int currentTimeBlock = 0;
 int minIdx = 0;
 int maxIdx = SIZE - 1;
 float squareSize = 0.0f;
-
-float kw = 1.2;
-float kv = 1.f;
-float wlimit = 3.f;
-float vlimit = 0.6;
-float erreurPos = 0.07;
 
 double reductionAngle(double x) {
 	x = fmod(x + PI, 2 * PI);
@@ -69,18 +63,21 @@ void printMaze() {
 double clamp(double x, double mini, double maxi) { return x < mini ? mini : x > maxi ? maxi : x; }
 
 void goTo(Position fromPos, Position toPos) {
+	static const float wlimit = 0.4;
+	static const float vlimit = 0.6;
+	static const float epsilon = 0.07;
 	double consvl, consvr;
 	double dx = toPos.x - fromPos.x;
 	double dy = toPos.y - fromPos.y;
 	double d = sqrt(dx * dx + dy * dy);
 
-	if (d > erreurPos) {
+	if (d > epsilon) {
 		double rho = atan2(dy, dx);
 		double angle = reductionAngle(rho - fromPos.a);
-		double consw = clamp(kw * angle, -wlimit, wlimit);
-		double consv = clamp(kv * d * cos(angle), -vlimit, vlimit);
-		consvl = consv - gladiator->robot->getRobotRadius() * consw;
-		consvr = consv + gladiator->robot->getRobotRadius() * consw;
+		double consw = clamp(12 * angle, -wlimit, wlimit);
+		double consv = clamp(d * cos(angle), -vlimit, vlimit);
+		consvl = consv - consw;
+		consvr = consv + consw;
 	} else {
 		consvl = 0;
 		consvr = 0;
@@ -94,6 +91,7 @@ void reset() {
 	frameCount = 0;
 	squareSize = gladiator->maze->getSquareSize();
 	teamId = gladiator->robot->getData().teamId;
+	gladiator->log("%d", teamId);
 	for (int y = 0; y < SIZE; ++y) {
 		for (int x = 0; x < SIZE; ++x) {
 			const MazeSquare* mazeSquare = gladiator->maze->getSquare(x, y);
@@ -103,6 +101,7 @@ void reset() {
 			maze[y][x][WEST] = !!mazeSquare->westSquare;
 		}
 	}
+	// printMaze();
 }
 
 void setup() {
@@ -116,6 +115,7 @@ int getScore(int x, int y, t_dir dir) {
 		(dir == SOUTH && !maze[y][x][NORTH]) || (dir == WEST && !maze[y][x][EAST]))
 		return -1;
 	const MazeSquare* mazeSquare = gladiator->maze->getSquare(x, y);
+	if (dir == ROCKET && mazeSquare->possession == teamId) return -1;
 	return mazeSquare->coin.value * 3 + (mazeSquare->possession == teamId ? 0
 										 : mazeSquare->possession == 0	  ? 1
 																		  : 2);
@@ -129,7 +129,7 @@ void loop() {
 			auto deltaTime =
 				std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime)
 					.count();
-			int newTimeBlock = deltaTime / 20000;
+			int newTimeBlock = (deltaTime + 2000) / 20000;
 			if (newTimeBlock != currentTimeBlock) {
 				currentTimeBlock = newTimeBlock;
 				++minIdx;
@@ -140,16 +140,14 @@ void loop() {
 		Position myPosition = gladiator->robot->getData().position;
 		int x = (int)(myPosition.x / squareSize);
 		int y = (int)(myPosition.y / squareSize);
-		int neighbors[4][3] = {
-			{x, y + 1, NORTH},
-			{x + 1, y, EAST},
-			{x, y - 1, SOUTH},
-			{x - 1, y, WEST},
+		int neighbors[5][3] = {
+			{x, y, ROCKET},	   {x, y + 1, NORTH}, {x + 1, y, EAST},
+			{x, y - 1, SOUTH}, {x - 1, y, WEST},
 		};
-		int bestX = SIZE >> 1;
-		int bestY = SIZE >> 1;
+		int bestX = -1;
+		int bestY = -1;
 		int bestScore = -1;
-		for (uint8_t i = 0; i < 4; ++i) {
+		for (uint8_t i = 0; i < 5; ++i) {
 			int score = getScore(neighbors[i][0], neighbors[i][1], (t_dir)neighbors[i][2]);
 			if (score > bestScore) {
 				bestScore = score;
@@ -157,13 +155,24 @@ void loop() {
 				bestY = neighbors[i][1];
 			}
 		}
+		if (bestScore == -1) {
+			bestX = SIZE >> 1;
+			bestY = SIZE >> 1;
+			for (int tryY = minIdx; tryY <= maxIdx; ++tryY) {
+				for (int tryX = minIdx; tryX <= maxIdx; ++tryX) {
+					int score = getScore(tryX, tryY, ROCKET);
+					if (score > bestScore) {
+						bestScore = score;
+						bestX = tryX;
+						bestY = tryY;
+					}
+				}
+			}
+		}
 		float targetX = ((float)bestX + 0.5f) * squareSize;
 		float targetY = ((float)bestY + 0.5f) * squareSize;
-		float dx = targetX - myPosition.x;
-		float dy = targetY - myPosition.y;
-		Position goal{targetX, targetY, atan2(dy, dx)};
+		Position goal{targetX, targetY, 0};
 		goTo(myPosition, goal);
-		// gladiator->log("%.4f", gladiator->maze->getSize());
-		// gladiator->log("%.4f", gladiator->maze->getSquareSize());
+		delay(5);
 	}
 }
